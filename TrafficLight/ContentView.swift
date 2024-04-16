9c4f1a1f216641ce1e705e36eac60be1510b3819
vsvami <106614918+vsvami@users.noreply.github.com>
2024-04-16 13:42:03 +0400

    Initial Commit

---
+++ TrafficLight/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrafficLight
+//
+//  Created by Vladimir Dmitriev on 16.04.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Vladimir Dmitriev on 16.04.24.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
